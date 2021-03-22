d84b449e6b54e872ad28839f6fda49bc8a49a0af
Сергей Дроздов <80007194+Drozd-off@users.noreply.github.com>
2021-03-23 00:00:08 +0500

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,24 @@
+"""   Modul for messages   """
+
+
+start_text = """ 🤚🏼 Здравствуйте, {}!
+
+Меня называют FIRST_BOT и я очень полезен! 
+
+🇷🇺 введите 1️⃣ Выберите русский язык.
+🇺🇿 kiriting 2️⃣ O'zbek tilni tanlaysizmi.
+🇬🇧 enter 3️⃣  Choose english language.           """
+
+
+ru_hi = """  🙋🏻 Привет!   """
+ru_bye = """    🙎🏻 Пока!     """
+ru_age = """    🤷🏻 Незнаю если честно...     """
+
+
+en_hi = """   🙋🏻 Hello!   """
+en_bye = """    🙎🏻 Goodbye!      """
+en_age = """    🤷🏻 To be honest, I don’t know...     """
+
+uz_hi = """   🙋🏻 Salom!   """
+uz_bye = """    🙎🏻 Xayr!      """
+uz_age = """    🤷🏻 Rostini aytsam, bilmayman...     """
